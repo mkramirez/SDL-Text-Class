fd99c480d98fcdbd7069535ab5539f51fdefe601
da-be-at <mkramirez1992@gmail.com>
2017-12-06 13:18:10 -0800

    Initial Commit

---
+++ LUL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LUL
+//
+//  Created by Matthew Ramirez on 12/6/17.
+//  Copyright © 2017 Matthew Ramirez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
